5f360cf75ee6aa311ec9479187b0c11e657f24be
Rafinhas <rafarrsobrinho@hotmail.com>
2022-09-23 19:03:41 -0300

    novas coisas

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -2,6 +2,7 @@ import './App.scss'
 import PokeCardsContainer from '../Components/PokeCardContainer'
 import Header from '../Components/Header'
 import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       <div className="App__maincontainer">
         <Navbar />
         <PokeCardsContainer />
+        <Footer/>
       </div>
     </div>
   )
---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return ( <div className='footer'>
+        <input type="button" value='Carregar mais pokemons'/>
+    </div> )
+}
+
+// fazer gif de carregamento do lado do botao, fazer aparecer quando clicar e qnd terminar de carregar desaparecer
+
+export default Footer
